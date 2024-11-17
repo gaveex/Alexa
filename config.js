2f859431bc8f391a329ba3bbbebd5f2acdd3ba31
gaveex <gaveesharandith@gmail.com>
2024-11-17 12:44:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "SnhwzaRD#LLZfw-uyx4jojooxM7wGqHVUkKJu5eK5g0LwBJP-tuo",
+SESSION_ID: process.env.SESSION_ID || "C3YHVaxT#zb7bAaNHXvX2GLLt5i6subNxIiEFTqkecNUYdW6mmrg",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "private",
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
+MODE: process.env.MODE || "public",
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
 };
